d6c56676958611e17f37aff61d8b719b00106a9f
Tdude <tibbemail@gmail.com>
2024-12-09 22:29:55 +0100

    Collapsible boxes FTW

--- public/js/ra-public.js
+++ public/js/ra-public.js
@@ -1,1 +1,19 @@
-// for future script
+// for PUBLIC script
+window.RAPublicUtils = {
+  initCollapsible: function () {
+    document.querySelectorAll(".ra-collapsible-title").forEach((title) => {
+      title.addEventListener("click", function () {
+        const content = document.getElementById(this.dataset.target);
+        const isVisible = content.classList.contains("show");
+
+        // Toggle content
+        content.classList.toggle("show");
+        this.classList.toggle("active");
+      });
+    });
+  },
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  RAPublicUtils.initCollapsible();
+});
